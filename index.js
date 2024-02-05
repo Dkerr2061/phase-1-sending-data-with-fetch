c3d39b05da6b5237a07b9560c18215eef23e56b0
David Kerr <kerrky@gmail.com>
2024-02-04 20:07:45 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    })
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    document.body.append(data.id)
+     
+  })
+  .catch(function(error) {
+    alert('Bad News!')
+    document.body.append(error.message)
+  })
+}
+
